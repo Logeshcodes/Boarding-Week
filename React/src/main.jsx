5788be5819a4e152e0c35d3a3beb3c47fa4fe0b6
Logesh <logeshc18022002@gmail.com>
2025-04-20 15:16:00 +0530

    removed

--- React/src/main.jsx
+++ React/src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-// import App from './App.jsx'
+
 import State from './Hooks/State.jsx'
-import TwoWayBinding from './Props/TwoWayBinding.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <State />
-    {/* <Form/> */}
-    {/* <Portal/> */}
   </StrictMode>,
 )
